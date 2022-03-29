572571e41b243d2adf0a40fc698aac872288b57f
luccasl <luccasleite@hotmail.com>
2022-03-29 20:20:51 -0300

    (feat) Add pokemon card alt text

--- components/PokemonCard.tsx
+++ components/PokemonCard.tsx
@@ -24,7 +24,10 @@ const PokemonCard: FC<{
 
     return <div key={ id } className={`animate-fadeIn flex flex-col bg-gradient-to-tr from-${transformedColor}-500 to-${transformedColor}-500/50 shadow-md rounded-md aspect-square w-full pt-2 hover:shadow-lg transition-shadow duration-200 hover:cursor-pointer`}>
       <div className='flex-1 h-20'>
-        <img className={`h-full w-full object-contain ${missingArtwork && 'brightness-0'}`} src={ imageUrl } />
+        <img
+          className={`h-full w-full object-contain ${missingArtwork && 'brightness-0'}`}
+          src={ imageUrl }
+          alt={ name } />
       </div>
       <div className='flex justify-center pb-2 pt-2 text-white font-Montserrat'>
         { name }
